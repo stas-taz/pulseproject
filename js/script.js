ab52f5ce52ec27a9d74572015377e8f4b64258d9
unknown <danecpipec@gmal.com>
2020-07-08 21:35:13 +0300

    alert-done

--- js/script.js
+++ js/script.js
@@ -35,4 +35,21 @@ const slider = tns({
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__link-back');
+
+    //alert
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+
+    $('.catalog-item__btn').each(function(i) {
+      $(this).on('click', function() {
+        $('#order .modal__subtitle').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      })
+    });
 });
